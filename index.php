1321786dc3800743bd836d520325fd939efacfc6
yuhei.nagano <yuheinagano8942@gmail.com>
2019-01-26 04:57:14 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+$sh = 7;
+switch ($sh) {
+    case 0;
+     echo 'Hello PHP';
+    break;
+    
+    case 1;
+     echo 'Goodbye PHP';
+    break;
+    
+    case 2;
+     echo 'Adios PHP';
+    break;
+    
+    case 3;
+     echo 'Sayonara PHP';
+    break;
+    
+    default;
+     echo 'エラーが発生しました。';
+}
+?>
